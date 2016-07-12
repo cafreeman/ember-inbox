891eb4f086d3f3c3e6b0f9a29829d8751767b71d
Chris Freeman <cfreeman.code@gmail.com>
2016-07-12 11:24:00 -0500

    Set up SASS and add `bootswatch`

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: [
+        'bower_components/bootstrap-sass/assets/stylesheets',
+        'bower_components/bootswatch',
+      ],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
--- mirage/factories/message.js
+++ mirage/factories/message.js
@@ -1,7 +1,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  senderEmail() { return faker.internet.email() },
+  senderEmail() { return faker.internet.email(); },
   senderName() { return `${faker.name.firstName()} ${faker.name.lastName()}`; },
   subject() { return faker.lorem.sentence(); },
   body() { return faker.lorem.paragraphs(); },
