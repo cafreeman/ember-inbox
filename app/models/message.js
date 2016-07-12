685f22c312b2045ffc04ef489536293e31041e2d
Chris Freeman <cfreeman.code@gmail.com>
2016-07-12 08:43:18 -0500

    set up Message model in ember-cli-mirage

---
+++ app/models/message.js
@@ -0,0 +1,10 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  senderEmail: attr('string'),
+  senderName: attr('string'),
+  subject: attr('string'),
+  body: attr('string'),
+});
---
+++ mirage/factories/message.js
@@ -0,0 +1,8 @@
+import { Factory, faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  senderEmail() { return faker.internet.email() },
+  senderName() { return `${faker.name.firstName()} ${faker.name.lastName()}`; },
+  subject() { return faker.lorem.sentence(); },
+  body() { return faker.lorem.paragraphs(); },
+});
